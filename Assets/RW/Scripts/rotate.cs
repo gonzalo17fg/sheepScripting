f92d957beaf80706754e60fc2c203ac3199070ad
gonzalo17fg <gonzalo.fuentes01@estudiant.upf.edu>
2023-05-01 18:44:42 +0200

    FirstScripts

---
+++ Assets/RW/Scripts/rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public Vector3 rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationSpeed = new Vector3(0, 50, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+    }
+}
